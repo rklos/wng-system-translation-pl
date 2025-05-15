f2e8a3faeb39c7357055fead4f4a9fe77966805c
Radosław Kłos <radoslaw@klos.dev>
2025-05-15 18:13:27 +0200

    Override impossible to translate templates

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
+function log(message) {
+  console.log(`[WNG-System-Translation-PL] ${message}`);
+}
+
 function reorderSkills() {
-  /**
-   * Default Skills used by common actors
-   */
   const OriginalSkillsModel = SkillsModel;
   class SkillsModelPL extends OriginalSkillsModel {
     static defineSchema() {
@@ -65,7 +66,39 @@ function reorderSkills() {
   AgentSkillsModel = AgentSkillsModelPL;
 }
 
+function overrideTemplates() {
+  // TODO: If we implement build process, we should load all templates automatically
+  const templatePaths = [
+    'apps/item-traits.hbs',
+
+    'chat/roll/ability/ability-use.hbs',
+    'chat/roll/base/base-buttons.hbs',
+    'chat/roll/base/dice-container.hbs',
+    'chat/roll/common/common-buttons.hbs',
+    'chat/roll/corruption/corruption-buttons.hbs',
+    'chat/roll/damage/damage-roll.hbs',
+    'chat/roll/mutation/mutation-buttons.hbs',
+    'chat/roll/mutation/mutation-roll.hbs',
+    'chat/roll/power/power-buttons.hbs',
+    'chat/roll/weapon/weapon-buttons.hbs',
+
+    'dialog/attack-roll.hbs',
+  ];
+
+  templatePaths.forEach(async (path) => {
+    const originalPath = `systems/wrath-and-glory/template/${path}`;
+
+    // Hack to unregister original W&G system's templates
+    Handlebars.unregisterPartial(originalPath);
+    await loadTemplates({
+      [originalPath]: `modules/wng-system-translation-pl/template/${path}`
+    });
+    log(`Overridden template: ${originalPath}`);
+  });
+}
+
 Hooks.on('init', async () => {
   await new Promise(resolve => setTimeout(resolve, 250));
   reorderSkills();
+  overrideTemplates();
 });
