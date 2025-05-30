9027634465be92e40dcde053726fe0ed85006b30
Radosław Kłos <radoslaw@klos.dev>
2025-05-30 13:19:42 +0200

    Add custom styles for actor sheet

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,23 @@
 import copy from 'rollup-plugin-copy';
 import del from 'rollup-plugin-delete';
+import scss from 'rollup-plugin-scss';
 
 export default {
   input: 'src/scripts/main.js',
   output: {
-    dir: 'dist/scripts',
+    dir: 'dist',
+    entryFileNames: 'scripts/main.js',
+    assetFileNames: (asset) => {
+      if (asset.name.endsWith('.css')) return `styles/${asset.name}`;
+      return `assets/${asset.name}`;
+    },
   },
   plugins: [
     del({ targets: 'dist' }),
+    scss({
+      name: 'main.css',
+      silenceDeprecations: ['legacy-js-api'],
+    }),
     copy({
       targets: [
         { src: 'src/module.json', dest: 'dist/' },
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 import { reorderSkills } from './reorder-skills.js';
 import { patchTemplates } from './patch-templates.js';
+import '../styles/main.scss';
 
 Hooks.on('init', async () => {
   // Without waiting at the beginning because we need to be faster than the system's init
