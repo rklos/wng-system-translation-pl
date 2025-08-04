8c8b5e2e1f602908437438601bb14a07bedeb79c
Radosław Kłos <radoslaw@klos.dev>
2025-08-04 12:42:34 +0200

    Sync whlib lang file

--- src/packages/warhammer-library/index.ts
+++ src/packages/warhammer-library/index.ts
@@ -1,5 +1,5 @@
 export const PACKAGE = 'warhammer-library';
 export const REPO = 'moo-man/WarhammerLibrary-FVTT';
-export const SUPPORTED_VERSION = '2.2.0';
+export const SUPPORTED_VERSION = '2.3.0';
 
 export function init() {}
