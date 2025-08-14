3dd13f9103aa6a84a87492bdfc1877ea2aa7884a
Radosław Kłos <radoslaw@klos.dev>
2025-08-14 08:23:18 +0200

    Update supported WHLib version

--- src/packages/warhammer-library/index.ts
+++ src/packages/warhammer-library/index.ts
@@ -1,5 +1,5 @@
 export const PACKAGE = 'warhammer-library';
 export const REPO = 'moo-man/WarhammerLibrary-FVTT';
-export const SUPPORTED_VERSION = '2.3.0';
+export const SUPPORTED_VERSION = '2.3.1';
 
 export function init() {}
